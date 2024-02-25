fcd018a993678928606828530e658471ec865b06
joshsonofmichael <107602793+joshsonofmichael@users.noreply.github.com>
2024-02-24 18:34:54 -0700

    switched and renamed everything in new branch

---
+++ Mission07_Michaelson/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mission07_Michaelson.Models;
+
+namespace Mission07_Michaelson.Controllers;
+
+public class HomeController : Controller
+{
+    // private readonly ILogger<HomeController> _logger;
+
+    // public HomeController(ILogger<HomeController> logger)
+    // {
+    //     _logger = logger;
+    // }
+    private MoviesContext _context; // set up instance of database
+    public HomeController(MoviesContext temp) 
+    {
+        _context = temp; // assign instance to the current database
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult AboutJoel()
+    {
+        return View();
+    }
+    [HttpGet]
+    public IActionResult AddMovie()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult AddMovie(Movie movie)
+    {
+        _context.Movies.Add(movie); // add record to database
+        _context.SaveChanges();
+        return View("Success", movie);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
